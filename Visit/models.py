4ab82f7b7154c6b650820ceea8c347f859c4ee16
nka20 <ntwarilevinkenallan@gmail.com>
2024-01-26 12:54:04 +0200

    pull

--- Visit/models.py
+++ Visit/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-# from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 # from multiupload.fields import MultiImageField
 # Create your models here.
